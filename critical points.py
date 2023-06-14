5c657f723b17299900b9edc6d400b9f715f3c3b7
Vanturos <uliyara007@gmail.com>
2023-06-14 17:13:51 +0300

    Latest

--- critical points.py
+++ critical points.py
@@ -23,41 +23,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-K = float(input('Введите коэффициент связи:'))
-
-n = 4
-f0 = 1
-sigma = 0.15
-tmax = 500
-dt = 0.01
-K1 = 4*K
-K2 = K
-
-f = np.zeros((n, int(tmax/dt)))
-
-f[:,0] = np.random.normal(f0, sigma*f0, n)
-
-f_2 = np.zeros((n, int(tmax / dt)))
-
-f_2[:, 0] = np.random.normal(f0, sigma * f0, n)
-
 def kuramoto1(f, K1, K2):
     dfdt = np.zeros(n)
     for i in range(n):
         for j in range(n):
             dfdt[i] += K1*(j>i)*np.sin(f[j]-f[i]) + K2*(j<i)*np.sin(f[j]-f[i])
     return dfdt
-
-for i in range(len(f[0])-1):
-    f[:,i+1] = f[:,i] + kuramoto1(f[:,i], K1, K2)*dt
-
-print('Для случая все - все:')
-for i in range(len(f[0]) - 1):
-    a, b, c, d = f[:, i]
-    x = 0.001
-    if abs(a-b)<x and abs(b-c)<x and abs(c-d)<x and abs(d-a)<x:
-        print("Критическая точка = {}\nЧастота в ней = {}".format(round(dt*(i+1),2), round(a, 2)))
-        break
 
 def kuramoto2(f, K1):
     dfdt = np.zeros(n)
@@ -66,28 +37,44 @@ def kuramoto2(f, K1):
             dfdt[i] += K1*(j==0)*np.sin(f[j]-f[i])
     return dfdt
 
-for i in range(len(f_2[0]) - 1):
-    f_2[:, i + 1] = f_2[:, i] + kuramoto2(f_2[:, i], K1) * dt
+for qwer in range(0, 1000000):
+    K = qwer/100
+    n = 4
+    f0 = 1
+    sigma = 0.15
+    tmax = 500
+    dt = 0.01
+    K1 = 4*K
+    K2 = K
 
-print('\nДля случая один - все:')
-for i in range(len(f_2[0]) - 1):
-    a, b, c, d = f_2[:, i]
-    x = 0.001
-    if abs(a-b)<x and abs(b-c)<x and abs(c-d)<x and abs(d-a)<x:
-        print("Критическая точка = {}\nЧастота в ней = {}".format(round(dt*(i+1),2), round(a, 2)))
-        break
+    f = np.zeros((n, int(tmax/dt)))
 
-fig,(ax1, ax2) = plt.subplots(2)
-ax1.set_title("График зависимости частоты от времени для случая все - все")
-ax2.set_title("График зависимости частоты от времени для случая один - все")
-fig.set_size_inches(18.5, 10.5)
-plt.subplots_adjust(wspace=0.3, hspace=0.3)
-for i in range(n):
-    ax1.plot(np.arange(0,tmax,dt), f[i,:], label='гребец {}'.format(i+1))
-for i in range(n):
-    ax2.plot(np.arange(0,tmax,dt), f_2[i, :], label='гребец {}'.format(i + 1))
-ax1.set(xlabel='Время, t', ylabel='Частота опускания весла, 1/с')
-ax2.set(xlabel='Время, t', ylabel='Частота опускания весла, 1/с')
-plt.legend()
-plt.show()
+    f[:,0] = np.random.normal(f0, sigma*f0, n)
 
+    f_2 = np.zeros((n, int(tmax / dt)))
+
+    f_2[:, 0] = np.random.normal(f0, sigma * f0, n)
+
+    for i in range(len(f[0])-1):
+        f[:,i+1] = f[:,i] + kuramoto1(f[:,i], K1, K2)*dt
+
+    for i in range(len(f[0]) - 1):
+        a, b, c, d = f[:, i]
+        x = 0.001
+        if abs(a-b)<x and abs(b-c)<x and abs(c-d)<x and abs(d-a)<x:
+            break
+    else:
+        print('Для случая все - все:')
+        print('нет синхронизации', K)
+
+    for i in range(len(f_2[0]) - 1):
+        f_2[:, i + 1] = f_2[:, i] + kuramoto2(f_2[:, i], K1) * dt
+
+    for i in range(len(f_2[0]) - 1):
+        a, b, c, d = f_2[:, i]
+        x = 0.001
+        if abs(a-b)<x and abs(b-c)<x and abs(c-d)<x and abs(d-a)<x:
+            break
+    else:
+        print('Для случая один - все:')
+        print('нет синхронизации', K)
